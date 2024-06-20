11dfea346b31185daf167054af73a4e20dabf0ea
foreverealize <60579848+FOREVEREALIZE@users.noreply.github.com>
2024-06-20 12:50:00 +0200

    Fix stupid build error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import "@/styles/globals.css";
 import type {AppProps} from "next/app";
 import {ThemeProvider} from "@/components/theme-provider";
 import {StoreProvider} from "@/lib/store-context";
-import Store from "@/lib/store";
-
-const store = new Store()
 
 export default function App({Component, pageProps}: AppProps) {
     return (
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-            <StoreProvider store={store}>
+            <StoreProvider>
                 <Component {...pageProps} />
             </StoreProvider>
         </ThemeProvider>
